35d370231f409afe0f7b68d91aed95de32e912b3
contatodrmarcosgama-alt <contatodrmarcosgama@gmail.com>
2026-09-14 06:21:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Faz o app carregar instantaneamente (cara de aplicativo) e funcionar
 // mesmo com conexão instável, guardando uma cópia local dos arquivos.
 
-const CACHE_NAME = 'hfp-cache-v45';
+const CACHE_NAME = 'hfp-cache-v5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
