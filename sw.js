4095f01fc4f6ed0f5c7637dc8036e897e2799cab
contatodrmarcosgama-alt <contatodrmarcosgama@gmail.com>
2026-09-19 17:33:35 -0300

    Startup images de todos os iPhones + cache v8.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Faz o app carregar instantaneamente (cara de aplicativo) e funcionar
 // mesmo com conexão instável, guardando uma cópia local dos arquivos.
 
-const CACHE_NAME = 'hfp-cache-v7';
+const CACHE_NAME = 'hfp-cache-v8';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
