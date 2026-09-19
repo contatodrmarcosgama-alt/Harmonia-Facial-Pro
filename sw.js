b5d16849562a49da40cae02d406256bd83d64d81
contatodrmarcosgama-alt <contatodrmarcosgama@gmail.com>
2026-09-19 14:45:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Faz o app carregar instantaneamente (cara de aplicativo) e funcionar
 // mesmo com conexão instável, guardando uma cópia local dos arquivos.
 
-const CACHE_NAME = 'hfp-cache-v6';
+const CACHE_NAME = 'hfp-cache-v7';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
